31acfa95a66f17d51034c9f301157896c6136119
BernatMG <bernatmago@gmail.com>
2021-06-17 21:03:39 +0200

    updated resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -15,7 +15,7 @@ const greeting = {
     "A passionate Artificial Inteligence engineer & Back-End developer."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/12Jrdp9UQHkcNEFuzzcu4Kzee4YKoQgHp/view?usp=sharing",
+    "https://drive.google.com/file/d/1D1_NkEW3RTmKEAjSkrLtgYvsK7bwc5CC/view?usp=sharing",
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
@@ -198,7 +198,7 @@ To know how to get github key look at readme.md */
 
 const openSource = {
   showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
-  display: false // Set false to hide this section, defaults to true
+  display: true // Set false to hide this section, defaults to true
 };
 
 // Some big projects you have worked on
