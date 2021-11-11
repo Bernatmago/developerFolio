6c1879f27df2fd574651e9c7f571e4fdf5aa4230
Bernat <bolicubo@gmail.com>
2021-11-11 23:25:01 +0100

    Updates

---
+++ precache-manifest.ba5914de3d47ff1f39c4bfa63c0b6569.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94290f14765b46a95482a2eba478029",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "6c6218bf29ee5fa7d3c6",
+    "url": "/developerFolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "7e4d26148fa2ee43de0f",
+    "url": "/developerFolio/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "3bcf9da9a5c8a6f799d6",
+    "url": "/developerFolio/static/css/main.dcf76762.chunk.css"
+  },
+  {
+    "revision": "c54c807f1410dab39b6c",
+    "url": "/developerFolio/static/js/2.5ad252a1.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/developerFolio/static/js/2.5ad252a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6218bf29ee5fa7d3c6",
+    "url": "/developerFolio/static/js/3.561e6790.chunk.js"
+  },
+  {
+    "revision": "7e4d26148fa2ee43de0f",
+    "url": "/developerFolio/static/js/4.09e78f1c.chunk.js"
+  },
+  {
+    "revision": "3bcf9da9a5c8a6f799d6",
+    "url": "/developerFolio/static/js/main.d8b50810.chunk.js"
+  },
+  {
+    "revision": "2f02fd6c573800cd9a78",
+    "url": "/developerFolio/static/js/runtime-main.01a5c47d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "0f0ccd3c32b1d2f6ec9da7e65783caaa",
+    "url": "/developerFolio/static/media/bscLogo.0f0ccd3c.png"
+  },
+  {
+    "revision": "bcff15f27b4a16aa4979f12295875b35",
+    "url": "/developerFolio/static/media/chickenLogo.bcff15f2.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "762622943315f3a3feb6c20090a1169f",
+    "url": "/developerFolio/static/media/smLogo.76262294.jpg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/developerFolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "3c259fe9796ae8b094c152432803dbfe",
+    "url": "/developerFolio/static/media/uabLogo.3c259fe9.png"
+  },
+  {
+    "revision": "9d1d729160557e2ecb3bae4ec71f9969",
+    "url": "/developerFolio/static/media/upcLogo.9d1d7291.png"
+  }
+]);
